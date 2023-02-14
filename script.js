4f490c2043b24cf39090a3afac2ac97adee39159
Gabriel M (fun11) <71775534+Gabriel-fun11@users.noreply.github.com>
2023-02-14 19:48:13 -0300

    Tutorial done

---
+++ script.js
@@ -0,0 +1,11 @@
+function informacao() {
+    var info = prompt("SENHA:")
+
+    if (info == "concluido") {
+        window.open("informacao.html")
+    }
+}
+
+function error() {
+    alert("ERROR 404: NO PAGE FOUND.")
+}
